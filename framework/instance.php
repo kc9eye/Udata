c3b003dd4169cd1ab5da3888261262b1ca674fee
Paul W. Lane <kc9eye@outlook.com>
2019-07-16 13:16:27 -0500

    update notauthorized

--- framework/instance.php
+++ framework/instance.php
@@ -501,18 +501,22 @@ Class Instance {
         $buff = ob_get_contents();
         ob_clean();
         $this->loginRedirectHere();
+        $this->redirect('/user/login');
+        /*
         $view = $this->getViewer("Not Authorized!");
         ?>
-        <div class="row">
+        <!--<div class="row">
             <div class='col-md-3'></div>
             <div class="col-xs-12 col-md-6">
                 <img src='<?php echo $view->PageData['wwwroot'];?>/images/403.jpg' class='img-responsive' />
             </div>
             <div class='col-md-3'></div>
-        </div>
+        </div>-->
+
         <?php
         $view->footer();
         die();
+        */
     }
 
     /**
