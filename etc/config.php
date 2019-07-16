359064862cf4a52098098756cb1bf7da5dfa01d9
Paul W. Lane <kc9eye@outlook.com>
2019-07-16 13:26:18 -0500

    dev

--- etc/config.php
+++ etc/config.php
@@ -26,9 +26,9 @@
  */
 $config = [
     #Database configuration options
-    'dbuser' => 'dev',                                                      #Database user for the active db
+    'dbuser' => 'udata',                                                      #Database user for the active db
     'dbpass' => 'MonkeyFuck34',                                               #Database users password for the active db
-    'dbpdo' => 'pgsql:host=10.59.27.160;port=5432;dbname=udata;sslmode=prefer',  #PDO database connection string 'dbdriver:host=mydbhost;port=mydbport;dbnam=mydbname;[sslmode=mydbencryption...]
+    'dbpdo' => 'pgsql:host=localhost;port=5432;dbname=udata;sslmode=prefer',  #PDO database connection string 'dbdriver:host=mydbhost;port=mydbport;dbnam=mydbname;[sslmode=mydbencryption...]
 
     #Errorhandler config options
     'error-log-file-path' => '/var/www/udata/var/error_log.xml',    #Where to save th error log file, hard disk location (URL not supported at this time)
@@ -47,7 +47,7 @@ $config = [
     'mailer-custom-opts' => [],         #Custom SMTP options you may need to set for the mailer as an array
 
     #Application settings dependant on server settings
-    'application-root' => 'http://10.59.27.185:8080',         #The applications URL (Depends on how to machine is accessed what this should be.)
+    'application-root' => 'http://10.59.27.185',         #The applications URL (Depends on how to machine is accessed what this should be.)
     'data-root' => '/var/www/udata/data/raw',                  #Where data files are going to be stored. SDS files, images, etc... Must be writtable by the server process
     'template-root' => '/var/www/udata/wwwroot/templates',     #Where the application templates are stored for file_get_contents
     
