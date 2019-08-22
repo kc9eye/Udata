cefe6d55d0df25928bfb4dbb9ca73f16d53cb05e
Paul W. Lane <kc9eye@outlook.com>
2019-08-22 13:35:59 -0500

    regress configs

--- etc/config.php
+++ etc/config.php
@@ -26,19 +26,19 @@
  */
 $config = [
     #Database configuration options
-    'dbuser' => '',                                                      #Database user for the active db
-    'dbpass' => '',                                               #Database users password for the active db
-    'dbpdo' => '',  #PDO database connection string 'dbdriver:host=mydbhost;port=mydbport;dbnam=mydbname;[sslmode=mydbencryption...]
+    'dbuser' => 'dev',                                                      #Database user for the active db
+    'dbpass' => 'MonkeyFuck34',                                               #Database users password for the active db
+    'dbpdo' => 'pgsql:host=localhost;port=5432;dbname=udata;sslmode=prefer',  #PDO database connection string 'dbdriver:host=mydbhost;port=mydbport;dbnam=mydbname;[sslmode=mydbencryption...]
 
     #Errorhandler config options
-    'error-log-file-path' => '',    #Where to save th error log file, hard disk location (URL not supported at this time)
-    'error-support-link' => '', #A link to bug/issue reporting infrastructure, users will see this.
+    'error-log-file-path' => '/var/www/development/var/error_log.xml',    #Where to save th error log file, hard disk location (URL not supported at this time)
+    'error-support-link' => 'https://github.com/kc9eye/UData/issues', #A link to bug/issue reporting infrastructure, users will see this.
 
     #PHPMailer configuration
     'mailer-type' => 'SMTP',            #The mailer to use, SMTP (sends as SMTP), Mail (sends with PHP mail function), Sendmail (uses host sendmail server)
     'mailer-default-from-addr' => 'noreply@nowhere.com',   #The address the server will send mail as if not defined at send time
     'mailer-default-from-name' => 'UData Server',   #The name the server will send mail as if not defined at send time
-    'mailer-host' => '',                #comma separated list is acceptable in order of percedence
+    'mailer-host' => 'smtp.uhi.amerco',                #comma separated list is acceptable in order of percedence
     'mailer-SMTPAuth' => false,         #If this is set to true then the security type should also be set
     'mailer-username' => '',            #Required if above is true
     'mailer-password' => '',            #Same as above
@@ -47,14 +47,14 @@ $config = [
     'mailer-custom-opts' => [],         #Custom SMTP options you may need to set for the mailer as an array
 
     #Application settings dependant on server settings
-    'application-root' => '',         #The applications URL (Depends on how to machine is accessed what this should be.)
-    'data-root' => '/data/raw',                  #Where data files are going to be stored. SDS files, images, etc... Must be writtable by the server process
-    'template-root' => '/wwwroot/templates',     #Where the application templates are stored for file_get_contents
+    'application-root' => 'http://10.59.27.160/development',         #The applications URL (Depends on how to machine is accessed what this should be.)
+    'data-root' => '/var/www/development/data/raw',                  #Where data files are going to be stored. SDS files, images, etc... Must be writtable by the server process
+    'template-root' => '/var/www/development/wwwroot/templates',     #Where the application templates are stored for file_get_contents
     
     #ViewMaker pagedata settings, effect how the UI looks
-    'company-name' => 'Title Goes Here',
-    'company-motto' => 'Slogan goes here',
-    'home-name' => 'Home',
+    'company-name' => 'Chicago Assembly Division(TESTING)',
+    'company-motto' => 'Building Uhaul\'s Finest and Safest',
+    'home-name' => 'Safety',
     'theme' => 'dark', #Not implemented as yet
 
     #Main navbar links
@@ -64,6 +64,7 @@ $config = [
         'Material' => '/material/main',
         'Maintenance' => '/maintenance/main',
         'Human Resources' => '/hr/main',
+        '[DEBUG]' => '/debug'
     ],
 
     #Background operations
