98a77447f2038469a2d8445061df5f0e68959eb2
Paul W. Lane <kc9eye@outlook.com>
2019-09-12 07:43:58 -0500

    database update schema

---
+++ var/dbupdate.php
@@ -0,0 +1,30 @@
+<?php
+$update_version = 0.1;
+
+$sql = [
+    'create table review_comments (
+        id varchar primary key,
+        uid varchar not null,
+        revid varchar not null,
+        comments text not null,
+        _date timestamp with time zone default now()
+    )',
+    'create table reviews (
+        id varchar primary key,
+        eid varchar not null,
+        start_date timestamp with time zone default now(),
+        end_date timestamp with time zone default (now() + \'7 days\'::interval),
+        meeting_date date,
+        uid varchar not null
+    )'
+];
+
+$inserts = [
+    'insert into perms values (:id,:name)'=>
+    [
+        [
+            ':id'=>uniqid(),
+            ':name'=>'initEmployeeReview'
+        ]
+    ]
+];
