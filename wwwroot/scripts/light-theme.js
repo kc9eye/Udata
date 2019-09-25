21786235c1a2081d41642b3f605da0c73ea1a70a
Paul W. Lane <kc9eye@outlook.com>
2019-09-25 14:48:13 -0500

    theme dev

---
+++ wwwroot/scripts/light-theme.js
@@ -0,0 +1,31 @@
+/* This file is part of UData.
+ * Copyright (C) 2019 Paul W. Lane <kc9eye@outlook.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+$(document).ready(function(){
+    //The navbar goes to default
+    $('nav').removeClass('navbar-inverse');
+    $('nav').addClass('navbar-default');
+
+    //Header and footer should be the same
+    $('#template-header').css({
+        "background-color":"rgb(235, 235, 235)",
+        "color": "rgb(35, 35, 35)"
+    });
+    $('.footer').css({
+        "background-color":"rgb(235, 235, 235)",
+        "color": "rgb(35, 35, 35)"
+    });
+});
