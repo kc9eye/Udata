64ca8c161af63301724f32a3edb8ba9bd59eca86
Paul W. Lane <kc9eye@outlook.com>
2019-11-07 09:57:36 -0600

    fixes #72,#73,#74

--- safety/sbm.php
+++ safety/sbm.php
@@ -17,10 +17,20 @@
  */
 require_once(dirname(__DIR__).'/lib/init.php');
 
-$edit = $server->checkPermission('editSBM');
-
 if (!empty($_REQUEST['action'])) {
     switch($_REQUEST['action']) {
+        case 'add':
+            if (!$server->checkPermsArray(['editSBM','adminAll'])) $server->notAuthorized(true);
+            uploadNewMinutes();
+        break;
+        case 'delete':
+            if (!$server->checkPermsArray(['editSBM','adminAll'])) $server->notAuthorized(true);
+            $server->processingDialog(
+                [new SafetyBoardMinutes($server->pdo,$server->config['data-root']),'deleteSBMFileEntry'],
+                [$_REQUEST],
+                $server->config['application-root'].'/safety/sbm'
+            );
+        break;
         default: main(); break;
     }
 }
@@ -31,13 +41,52 @@ else {
 function main () {
     global $server;
     include('submenu.php');
-
+    $edit = $server->checkPermsArray(['editSBM','adminAll']);
     $sbm = new SafetyBoardMinutes($server->pdo,$server->config['data-root']);
     $view = $server->getViewer('Safety Board Minutes');
     $view->sideDropDownMenu($submenu);
     $view->h1('Safety Meeting Minutes');
+    if ($edit) {
+        $form = new InlineFormWidgets($view->PageData['wwwroot'].'/scripts');
+        $view->bold('Upload Minutes:(<2MB)');
+        $view->insertTab();
+        $form->newInlineMultipartForm();
+        $form->hiddenInput('action','add');
+        $form->hiddenInput('uid',$server->currentUserID);
+        $form->inlineInputCapture('date','Meeting Date',null,['dateISO'=>'true']);
+        $view->insertTab();
+        $form->inlineFileUpload(FileIndexer::UPLOAD_NAME,false,true);
+        $form->inlineSubmit('Upload');
+        $form->endInlineForm();
+    }
     $th = $edit ? ['Meeting Date','Delete'] : ['Meeting Date'];
     $view->responsiveTableStart($th);
-    foreach 
+    foreach($sbm->getListing() as $row) {
+        echo "<tr><td><a href='{$view->PageData['approot']}/data/files?dis=inline&file={$row['file']}'>";
+        $view->formatUserTimestamp($row['meeting_date']);
+        echo " Meeting</a></td>";
+        if ($edit) echo "<td>".$view->trashBtnSm("/safety/sbm?action=delete&ref={$row['id']}:{$row['fid']}",true)."</td>";
+        echo "</tr>";
+    }
+    $view->responsiveTableclose();
     $view->footer();
+}
+
+function uploadNewMinutes () {
+    global $server;
+    try {
+        $upload = new FileUpload(FileIndexer::UPLOAD_NAME);
+    }
+    catch (UploadException $e) {
+        $server->newEndUserDialog(
+            $e->getMessage(),
+            DIALOG_FAILURE,
+            $server->config['application-root'].'/safety/sbm'
+        );
+    }
+    $server->processingDialog(
+        [new SafetyBoardMinutes($server->pdo,$server->config['data-root']), 'addNewSBMFile'],
+        [$_REQUEST, $upload],
+        $server->config['application-root'].'/safety/sbm'
+    );
 }
