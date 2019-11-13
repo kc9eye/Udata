0487fa0894bb8c25612068194dc92229f0d364c7
Paul W. Lane <kc9eye@outlook.com>
2019-11-13 12:09:21 -0600

    hotfix2

--- hr/feedback.php
+++ hr/feedback.php
@@ -92,27 +92,27 @@ function addNewComment () {
     $notify = new Notification($server->pdo,$server->mailer);
     try {
         $upload = new FileUpload(FileIndexer::UPLOAD_NAME);
+        if ($upload !== false) {
+            if ($upload->multiple) {
+                $server->newEndUserDialog(
+                    "Only one file may be associated with this entry.",
+                    DIALOG_FAILURE,
+                    $server->config['application-root'].'/hr/feedback?id='.$_REQUEST['eid']
+                );
+            }
+            $indexer = new FileIndexer($server->pdo,$server->config['data-root']);
+            if (($indexed = $indexer->indexFiles($upload,$_REQUEST['uid'])) !== false)
+                $_REQUEST['fid'] = $indexed[0];
+        }
+        else $_REQUEST['fid'] = '';
     }
     catch (UploadException $e) {
         if ($e->getCode() != UPLOAD_ERR_NO_FILE) {
             trigger_error($e->getMessage(),E_USER_WARNING);
             return false;
         }
-    }    
-
-    if ($upload !== false) {
-        if ($upload->multiple) {
-            $server->newEndUserDialog(
-                "Only one file may be associated with this entry.",
-                DIALOG_FAILURE,
-                $server->config['application-root'].'/hr/feedback?id='.$_REQUEST['eid']
-            );
-        }
-        $indexer = new FileIndexer($server->pdo,$server->config['data-root']);
-        if (($indexed = $indexer->indexFiles($upload,$_REQUEST['uid'])) !== false)
-            $_REQUEST['fid'] = $indexed[0];
+        $_REQUEST['fid'] = '';
     }
-    else $_REQUEST['fid'] = '';
 
     if ($handler->addNewSupervisorFeedback($_REQUEST)) {
         $body = file_get_contents(INCLUDE_ROOT.'/wwwroot/templates/email/supervisorfeedback.html');
