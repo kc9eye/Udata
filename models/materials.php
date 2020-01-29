3c4e7cd9906ece38c26eecea16eeb4a472f6431f
Paul Lane <kc9eye@outlook.com>
2020-01-29 13:39:01 -0600

    tyree bug

--- models/materials.php
+++ models/materials.php
@@ -414,8 +414,7 @@ class Materials {
                 type
             FROM discrepancies as a
             WHERE _date >= :begin
-            AND _date <= :end 
-            ORDER by partid";
+            AND _date <= :end";
         switch($type) {
             case 'PDIH': 
                 $sql .= " AND type = :type";
