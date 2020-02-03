c609430ac65d504890367d7410b44e6139727055
Paul Lane <kc9eye@outlook.com>
2020-02-03 07:23:18 -0600

    fuck

--- etc/config.php
+++ etc/config.php
@@ -47,7 +47,7 @@ $config = [
     'mailer-custom-opts' => [],         #Custom SMTP options you may need to set for the mailer as an array
 
     #Application settings dependant on server settings
-    'application-root' => 'http://10.59.0.133',         #The applications URL (Depends on how to machine is accessed what this should be.)
+    'application-root' => 'http://10.59.27.133',         #The applications URL (Depends on how to machine is accessed what this should be.)
     'data-root' => '/var/www/development/data/raw',                  #Where data files are going to be stored. SDS files, images, etc... Must be writtable by the server process
     'template-root' => '/var/www/development/wwwroot/templates',     #Where the application templates are stored for file_get_contents
     
