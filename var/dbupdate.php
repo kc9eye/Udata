7526c2e6b9e8aab98cb9dce44fd9bf92c88bc22f
Paul Lane <kc9eye@outlook.com>
2020-05-05 15:43:09 -0500

    fixes #92

--- var/dbupdate.php
+++ var/dbupdate.php
@@ -2,24 +2,7 @@
 $update_version = 0.19;
 
 $sql = [
-    'CREATE TABLE public.cell_prints
-    (
-        id character varying NOT NULL,
-        cellid character varying NOT NULL,
-        "number" character varying NOT NULL,
-        uid character varying NOT NULL,
-        _date timestamp with time zone DEFAULT now(),
-        PRIMARY KEY (id)
-    )',
-    'CREATE TABLE public.cell_files
-    (
-        id character varying NOT NULL,
-        fid character varying NOT NULL,
-        cellid character varying NOT NULL,
-        uid character varying NOT NULL,
-        _date timestamp with time zone DEFAULT now(),
-        PRIMARY KEY (id)
-    )'
+    'select CURRENT_TIME'
  ];
 
 $inserts = [
